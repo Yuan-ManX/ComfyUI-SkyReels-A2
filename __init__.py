ca60f824c47eb3b88594d14a6fc14972a319a51b
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-07 16:06:22 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
+from .nodes import A2ModelLoader, A2VideoGenerator, SaveMP4Video
 
+NODE_CLASS_MAPPINGS = {
+    "A2 Model Loader": A2ModelLoader,
+    "A2 Video Generator": A2VideoGenerator,
+    "Save MP4 Video": SaveMP4Video,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "A2ModelLoader": "A2 Model Loader",
+    "A2VideoGenerator": "A2 Video Generator",
+    "SaveMP4Video": "Save MP4 Video",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
